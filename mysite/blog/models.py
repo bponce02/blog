566a404dfedb89e5f782f4a449ad9c507ca569c8
Bryan Ponce <poncebryan002@gmail.com>
2025-10-13 21:05:47 -0400

    Add tags(tutorial)

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -1,8 +1,12 @@
 from django.db import models
 
+from modelcluster.fields import ParentalKey, ParentalManyToManyField
+from modelcluster.contrib.taggit import ClusterTaggableManager
+from taggit.models import TaggedItemBase
 # Add these:
-from wagtail.models import Page
+from wagtail.models import Page, Orderable
 from wagtail.fields import RichTextField
+from wagtail.admin.panels import FieldPanel, InlinePanel, MultiFieldPanel
 
 
 class BlogIndexPage(Page):
@@ -17,9 +21,39 @@ class BlogIndexPage(Page):
         context['blogpages'] = blogpages
         return context
 
+class BlogPageTag(TaggedItemBase):
+    content_object = ParentalKey(
+        'BlogPage',
+        related_name='tagged_items',
+        on_delete=models.CASCADE
+    )
 class BlogPage(Page):
     date = models.DateField("Post date")
     intro = models.CharField(max_length=250)
     body = RichTextField(blank=True)
+    
+    # Add this:
+    tags = ClusterTaggableManager(through=BlogPageTag, blank=True)
 
-    content_panels = Page.content_panels + ["date", "intro", "body"]
+    # ... Keep the main_image method. Then modify the content_panels:
+    content_panels = Page.content_panels + [
+        MultiFieldPanel([
+            "date",
+            # Add this:
+            "tags",
+        ], heading="Blog information"),
+            "intro", "body",
+        ]
+
+class BlogTagIndexPage(Page):
+
+    def get_context(self, request):
+
+        # Filter by tag
+        tag = request.GET.get('tag')
+        blogpages = BlogPage.objects.filter(tags__name=tag)
+
+        # Update template context
+        context = super().get_context(request)
+        context['blogpages'] = blogpages
+        return context
