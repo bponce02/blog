d831402ae7d20f8f5ccd72c6eea9aa8fe4593c98
Bryan Ponce <poncebryan002@gmail.com>
2025-10-19 15:43:28 -0400

    Add env variables

--- mysite/mysite/settings/production.py
+++ mysite/mysite/settings/production.py
@@ -1,6 +1,13 @@
 from .base import *
+import os
 
 DEBUG = False
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'change-me-in-production')
+
+# Add your domain here
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'develium.dev,www.develium.dev').split(',')
 
 # ManifestStaticFilesStorage is recommended in production, to prevent
 # outdated JavaScript / CSS assets being served from cache
